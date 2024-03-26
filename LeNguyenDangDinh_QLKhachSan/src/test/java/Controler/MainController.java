1741a34fea4a27988208f1d469dd4356dd2c96ab
Djnk2203 <131732391+Djnk2203@users.noreply.github.com>
2024-03-26 17:07:52 +0700

    up

--- LeNguyenDangDinh_QLKhachSan/src/test/java/Controler/MainController.java
+++ LeNguyenDangDinh_QLKhachSan/src/test/java/Controler/MainController.java
@@ -1,8 +1,16 @@
+package Controler;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import Model.Diem;
+import service.QuanLyDiemService;
 
 @Controller
 public class MainController {
@@ -90,4 +98,29 @@ public class MainController {
         students.add(new Student("Lê Văn C", "10C", "Khoa C", "Trường XYZ"));
         return students;
     }
+    @Autowired
+    private QuanLyDiemService quanLyDiemService;
+    @GetMapping("/")
+    public String index1(Model model) {
+        model.addAttribute("diems", quanLyDiemService.layDanhSachDiem());
+        return "index";
+    }
+
+    @GetMapping("/them-diem")
+    public String hienThiFormThem(Model model) {
+        model.addAttribute("diem", new Diem(null, null, 0));
+        return "form-them";
+    }
+
+    @PostMapping("/them-diem")
+    public String themDiem(Diem diem) {
+        quanLyDiemService.themDiem(diem);
+        return "redirect:/";
+    }
+    @GetMapping("/danh-sach-diem")
+    public String hienThiDanhSachDiem(Model model) {
+        model.addAttribute("diems", quanLyDiemService.layDanhSachDiem());
+        return "danh-sach-diem";
+    }
+   
 }
--- LeNguyenDangDinh_QLKhachSan/src/test/java/service/QuanLyDiemServiceImpl.java
+++ LeNguyenDangDinh_QLKhachSan/src/test/java/service/QuanLyDiemServiceImpl.java
@@ -1,5 +1,41 @@
 package service;
 
-public class QuanLyDiemServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import Model.Diem; 
+
+@Service
+public class QuanLyDiemServiceImpl implements QuanLyDiemService {
+
+    private List<Diem> danhSachDiem = new ArrayList<>();
+
+    public QuanLyDiemServiceImpl() {
+       
+        danhSachDiem.add(new Diem("D1", "Điểm 1", 10));
+        danhSachDiem.add(new Diem("D2", "Điểm 2", 15));
+        danhSachDiem.add(new Diem("D3", "Điểm 3", 20));
+    }
+
+    @Override
+    public List<Diem> layDanhSachDiem() {
+        return danhSachDiem;
+    }
+
+    @Override
+    public Diem timDiemTheoMa(String ma) {
+        for (Diem diem : danhSachDiem) {
+            if (diem.getMa().equals(ma)) {
+                return diem;
+            }
+        }
+        return null; 
+    }
+
+    @Override
+    public void themDiem(Diem diem) {
+        danhSachDiem.add(diem);
+    }
 }

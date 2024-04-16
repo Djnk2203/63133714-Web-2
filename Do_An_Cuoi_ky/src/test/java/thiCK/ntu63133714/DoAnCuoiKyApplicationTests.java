005d76a16187a171153343ab85af9164b803d49d
Djnk2203 <131732391+Djnk2203@users.noreply.github.com>
2024-04-16 15:59:45 +0700

    up

---
+++ Do_An_Cuoi_ky/src/test/java/thiCK/ntu63133714/DoAnCuoiKyApplicationTests.java
@@ -0,0 +1,13 @@
+package thiCK.ntu63133714;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnCuoiKyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ QuanlyChungCu/src/main/java/thiCK/ntu63133714/QuanlyChungCuApplication.java
@@ -0,0 +1,13 @@
+package thiCK.ntu63133714;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlyChungCuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlyChungCuApplication.class, args);
+	}
+
+}

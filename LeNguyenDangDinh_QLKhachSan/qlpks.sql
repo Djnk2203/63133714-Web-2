1bc3d87269a6eb3b726a057fe95516ef92fe5d62
Djnk2203 <131732391+Djnk2203@users.noreply.github.com>
2024-04-23 15:28:30 +0700

    up

---
+++ LeNguyenDangDinh_QLKhachSan/qlpks.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th4 23, 2024 lúc 10:27 AM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `qlpks`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hoadon`
+--
+
+CREATE TABLE `hoadon` (
+  `MaHD` int(11) NOT NULL,
+  `MaNV` int(11) NOT NULL,
+  `MaPhong` int(11) NOT NULL,
+  `NgayNhan` date NOT NULL,
+  `NgayTra` date DEFAULT NULL,
+  `SoNgay` int(11) NOT NULL,
+  `DonGiaPhong` decimal(10,2) NOT NULL,
+  `ThanhTien` decimal(10,2) NOT NULL,
+  `PhuThu` decimal(10,2) DEFAULT NULL,
+  `ChietKhau` decimal(10,2) DEFAULT NULL,
+  `TongTien` decimal(10,2) NOT NULL,
+  `HinhThucTT` varchar(50) NOT NULL,
+  `TrangThai` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhanvien`
+--
+
+CREATE TABLE `nhanvien` (
+  `MaNV` int(11) NOT NULL,
+  `TenNV` varchar(255) NOT NULL,
+  `NgaySinh` date NOT NULL,
+  `CMND` varchar(12) NOT NULL,
+  `TenDN` varchar(255) NOT NULL,
+  `MK` varchar(255) NOT NULL,
+  `TenTP` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `phong`
+--
+
+CREATE TABLE `phong` (
+  `MaPhong` int(11) NOT NULL,
+  `Tang` int(11) NOT NULL,
+  `LoaiPhong` varchar(50) NOT NULL,
+  `SoNguoi` int(11) NOT NULL,
+  `Gia` decimal(10,2) NOT NULL,
+  `HinhAnh` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `thuephong`
+--
+
+CREATE TABLE `thuephong` (
+  `MaHD` int(11) NOT NULL,
+  `MaNV` int(11) NOT NULL,
+  `MaPhong` int(11) NOT NULL,
+  `NgayNhan` date NOT NULL,
+  `NgayTra` date DEFAULT NULL,
+  `Tien` decimal(10,2) NOT NULL,
+  `DatOrNhan` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD PRIMARY KEY (`MaHD`),
+  ADD KEY `MaNV` (`MaNV`),
+  ADD KEY `MaPhong` (`MaPhong`);
+
+--
+-- Chỉ mục cho bảng `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  ADD PRIMARY KEY (`MaNV`);
+
+--
+-- Chỉ mục cho bảng `phong`
+--
+ALTER TABLE `phong`
+  ADD PRIMARY KEY (`MaPhong`);
+
+--
+-- Chỉ mục cho bảng `thuephong`
+--
+ALTER TABLE `thuephong`
+  ADD PRIMARY KEY (`MaHD`);
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD CONSTRAINT `hoadon_ibfk_1` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`),
+  ADD CONSTRAINT `hoadon_ibfk_2` FOREIGN KEY (`MaPhong`) REFERENCES `phong` (`MaPhong`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
